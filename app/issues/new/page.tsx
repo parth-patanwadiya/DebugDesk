399ef8278edc4fbbfccdb9b8a69c2d38eb8b6a53
Parth Patanwadiya <parthparthb9999@gmail.com>
2024-10-11 13:38:25 -0700

    Move inline function

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -11,6 +11,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { createIssueSchema } from '@/app/validationSchemas';
 import { z } from 'zod';
 import ErrorMessages from '@/app/components/ErrorMessages';
+import Spinner from '@/app/components/Spinner';
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
@@ -25,6 +26,18 @@ const NewIssuePage = () => {
   });
 
   const [error, setError] = useState('');
+  const [isSubmitting, setSubmitting] = useState(false);
+
+  const onSubmit = handleSubmit( async (data) => {
+    try {
+      setSubmitting(true);
+      await axios.post('/api/issues', data);
+      router.push('/issues');
+    } catch (error) {
+      setSubmitting(false);
+      setError('An unexpected error occured.');
+    }
+  });
 
   return (
     <div className='max-w-xl'>
@@ -35,14 +48,8 @@ const NewIssuePage = () => {
       )}
       <form 
         className='space-y-3' 
-        onSubmit={handleSubmit( async (data) => {
-          try {
-            await axios.post('/api/issues', data);
-            router.push('/issues');
-          } catch (error) {
-            setError('An unexpected error occured.');
-          }
-        })}>
+        onSubmit={onSubmit}
+      >
         <TextField.Root>
           <TextField.Input placeholder='Title' {...register('title')}/>
         </TextField.Root>
@@ -55,11 +62,11 @@ const NewIssuePage = () => {
           render={({ field }) =>
             <SimpleMDE placeholder='Description' {...field}/>
           }
-        />
+        />  
         <ErrorMessages>
           {errors.description?.message}
           </ErrorMessages>
-        <Button>Submit New Issue</Button>
+        <Button disabled={isSubmitting}>Submit New Issue { isSubmitting && <Spinner/>}</Button>
       </form>
     </div>
   )
