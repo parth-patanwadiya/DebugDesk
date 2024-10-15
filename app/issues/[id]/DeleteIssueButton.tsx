9e7e82cb3dae775f6e299b0f4e02627e48f923bd
Parth Patanwadiya <parthparthb9999@gmail.com>
2024-10-15 13:03:59 -0700

    Show a confirmation dialogue

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,16 +1,45 @@
+'use client'
+
 import { TrashIcon } from '@radix-ui/react-icons'
-import { Button } from '@radix-ui/themes'
+import { AlertDialog, Button, Flex } from '@radix-ui/themes'
 import Link from 'next/link'
 
 const DeleteIssueButton = ({ issueId } : { issueId: number }) => {
   return (
-    <Button color='red'>
-      <TrashIcon />
-      <Link href={`/issues/${issueId}`}>
-        Delete
-      </Link>
-    </Button>
-  )
+    <AlertDialog.Root>
+
+      <AlertDialog.Trigger>
+        <Button color='red'>
+          <TrashIcon />
+            Delete
+        </Button>
+      </AlertDialog.Trigger>
+
+      <AlertDialog.Content>
+        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+
+        <AlertDialog.Description>
+          Are you sure you want to delete this issue? This action cannot be reversed.
+        </AlertDialog.Description>
+
+        <Flex gap="3" mt="4" justify="end">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button color="red">
+              Delete
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+
+
+      </AlertDialog.Content>
+      
+    </AlertDialog.Root>
+  );
 }
 
 export default DeleteIssueButton
