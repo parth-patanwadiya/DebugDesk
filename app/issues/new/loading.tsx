10063b2b62e5b61ca21c46c83586115f37bd5bcd
Parth Patanwadiya <parthparthb9999@gmail.com>
2024-10-15 00:17:52 -0700

    Organized import statements

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,6 +1,6 @@
+import { Skeleton } from '@/app/components'
 import { Box } from '@radix-ui/themes'
-import Skeleton from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
+
 
 const LoadingNewIssuePage = () => {
   return (
