99138a146b9106d8e558e66bf70a1a7d9238e234
Parth Patanwadiya <parthparthb9999@gmail.com>
2024-10-15 14:44:29 -0700

    Remove duplicate skeletons

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -22,7 +22,7 @@ const IssuesPage = async () => {
           {issues.map(issue => (
             <Table.Row key={issue.id}>
               <Table.Cell>
-                <Link href={`issues/${issue.id}`}>
+                <Link href={`/issues/${issue.id}`}>
                   {issue.title}
                 </Link>
                 <div className="block md:hidden">
