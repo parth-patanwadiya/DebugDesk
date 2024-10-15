92014c7706184e2f0462a5860d64acbba932e640
Parth Patanwadiya <parthparthb9999@gmail.com>
2024-10-15 14:06:45 -0700

    Improve the user experience

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,68 +1,73 @@
 'use client'
 
+import { Spinner } from '@/app/components'
 import { TrashIcon } from '@radix-ui/react-icons'
-import { AlertDialog, AlertDialogRoot, Button, Flex } from '@radix-ui/themes'
+import { AlertDialog, Button, Flex } from '@radix-ui/themes'
 import axios from 'axios'
 import { useRouter } from 'next/navigation'
 import { useState } from 'react'
 
-const DeleteIssueButton = ({ issueId } : { issueId: number }) => {
+const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
 
   const router = useRouter();
   const [error, setError] = useState(false);
+  const [isDeleting, setDeleting] = useState(false);
 
   const deleteIssue = async () => {
     try {
-      await axios.delete('/api/issues/'+ issueId);
+      setDeleting(true);
+      await axios.delete('/api/issues/' + issueId);
       router.push('/issues');
       router.refresh();
     } catch (error) {
+      setDeleting(false);
       setError(true);
     }
   };
 
   return (
     <>
-    <AlertDialog.Root>
+      <AlertDialog.Root>
 
-      <AlertDialog.Trigger>
-        <Button color='red'>
-          <TrashIcon />
+        <AlertDialog.Trigger>
+          <Button color='red' disabled={isDeleting}>
+            <TrashIcon />
             Delete
-        </Button>
-      </AlertDialog.Trigger>
+            {isDeleting && <Spinner />}
+          </Button>
+        </AlertDialog.Trigger>
 
-      <AlertDialog.Content>
-        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
 
-        <AlertDialog.Description>
-          Are you sure you want to delete this issue? This action cannot be reversed.
-        </AlertDialog.Description>
+          <AlertDialog.Description>
+            Are you sure you want to delete this issue? This action cannot be reversed.
+          </AlertDialog.Description>
 
-        <Flex gap="3" mt="4" justify="end">
-          <AlertDialog.Cancel>
-            <Button variant="soft" color="gray">
-              Cancel
-            </Button>
-          </AlertDialog.Cancel>
-          <AlertDialog.Action>
-            <Button color="red" onClick={ deleteIssue }>
-              Delete
-            </Button>
-          </AlertDialog.Action>
-        </Flex>
+          <Flex gap="3" mt="4" justify="end">
+            <AlertDialog.Cancel>
+              <Button variant="soft" color="gray">
+                Cancel
+              </Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button color="red" onClick={deleteIssue}>
+                Delete
+              </Button>
+            </AlertDialog.Action>
+          </Flex>
 
-      </AlertDialog.Content>
+        </AlertDialog.Content>
 
-    </AlertDialog.Root>
+      </AlertDialog.Root>
 
-    <AlertDialog.Root open={error}>
-      <AlertDialog.Content>
-        <AlertDialog.Title>Error</AlertDialog.Title>
-        <AlertDialog.Description>This issue could not be deleted.</AlertDialog.Description>
-        <Button mt="3" color="gray" variant='soft' onClick={()=> setError(false)} >OK</Button>
-      </AlertDialog.Content>
-    </AlertDialog.Root>
+      <AlertDialog.Root open={error}>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Error</AlertDialog.Title>
+          <AlertDialog.Description>This issue could not be deleted.</AlertDialog.Description>
+          <Button mt="3" color="gray" variant='soft' onClick={() => setError(false)} >OK</Button>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
     </>
   );
 }
