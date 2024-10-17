95ff43e822cdd94a337832ab70985cb3ebc5a01a
Parth Patanwadiya <parthparthb9999@gmail.com>
2024-10-17 16:01:42 -0700

    refactor the navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -9,69 +9,79 @@ import { useSession } from 'next-auth/react'
 import { Avatar, Box, Container, DropdownMenu, Flex, Text } from '@radix-ui/themes';
 
 const NavBar = () => {
-  const currPath = usePathname();
-  const { status, data: session } = useSession();
-
-  const links = [
-    { label: 'Dashboard', href: '/' },
-    { label: 'Issues', href: '/issues/list' }
-  ]
   return (
     <nav className='mb-6 px-5 py-4 border-b'>
       <Container>
         <Flex justify="between">
           <Flex align="center" gap="3">
             <Link href="/"><FaBug /></Link>
-            <ul className='flex space-x-6'>
-              {
-                links.map(link => <li key={link.href}>
-                  <Link
-                    className={classNames({
-                      'text-zinc-950': currPath === link.href,
-                      'text-zinc-500': currPath !== link.href,
-                      'hover:text-zinc-800 transition-colors': true
-                    })}
-                    href={link.href}>{link.label}</Link></li>)
-              }
-            </ul>
+            <NavLinks />
           </Flex>
-          <Box>
-            {
-              status === 'authenticated' && (
-                <DropdownMenu.Root>
-                  <DropdownMenu.Trigger>
-                    <Avatar
-                      src={session.user?.image!}
-                      fallback="?"
-                      size="2"
-                      radius='full'
-                      className='cursor-pointer'
-                    />
-                  </DropdownMenu.Trigger>
-                  <DropdownMenu.Content>
-                    <DropdownMenu.Label>
-                      <Text size="2">
-                        {session.user?.email}
-                      </Text>
-                    </DropdownMenu.Label>
-                    <DropdownMenu.Item>
-                      <Link href="/api/auth/signout">Logout</Link>
-                    </DropdownMenu.Item>
-                  </DropdownMenu.Content>
-                </DropdownMenu.Root>
-              )
-            }
-            {
-              status === 'unauthenticated' && (
-                <Link href="/api/auth/signin">Login</Link>
-
-              )
-            }
-          </Box>
+          <AuthStatus />
         </Flex>
       </Container>
     </nav>
-  )
-}
+  );
+};
+
+const NavLinks = () => {
+  const currPath = usePathname();
+
+  const links = [
+    { label: 'Dashboard', href: '/' },
+    { label: 'Issues', href: '/issues/list' }
+  ]
+  return (
+    <ul className='flex space-x-6'>
+      {
+        links.map(link => <li key={link.href}>
+          <Link
+            className={classNames({
+              'nav-link' : true,
+              '!text-zinc-950': currPath === link.href,
+            })}
+            href={link.href}>{link.label}</Link></li>)
+      }
+    </ul>
+  );
+};
+
+const AuthStatus = () => {
+  const { status, data: session } = useSession();
+  return (
+    <Box>
+      {
+        status === 'authenticated' && (
+          <DropdownMenu.Root>
+            <DropdownMenu.Trigger>
+              <Avatar
+                src={session.user?.image!}
+                fallback="?"
+                size="2"
+                radius='full'
+                className='cursor-pointer'
+              />
+            </DropdownMenu.Trigger>
+            <DropdownMenu.Content>
+              <DropdownMenu.Label>
+                <Text size="2">
+                  {session.user?.email}
+                </Text>
+              </DropdownMenu.Label>
+              <DropdownMenu.Item>
+                <Link href="/api/auth/signout">Logout</Link>
+              </DropdownMenu.Item>
+            </DropdownMenu.Content>
+          </DropdownMenu.Root>
+        )
+      }
+      {
+        status === 'unauthenticated' && (
+          <Link href="/api/auth/signin" className='nav-link'>Login</Link>
+        )
+      }
+    </Box>
+  );
+};
 
 export default NavBar
