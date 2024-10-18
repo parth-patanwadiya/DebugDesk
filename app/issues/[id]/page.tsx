230a95610e2f91d23fd95915e6a5a6d40904be6a
Parth Patanwadiya <parthparthb9999@gmail.com>
2024-10-18 13:17:55 -0700

    Assign an issue to a user

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,45 +1,42 @@
-import prisma from '@/prisma/client';
-import { Box, Flex, Grid } from '@radix-ui/themes';
-import { notFound } from 'next/navigation';
-import EditIssueButton from './EditIssueButton';
-import IssueDetails from './IssueDetails';
-import DeleteIssueButton from './DeleteIssueButton';
-import { getServerSession } from 'next-auth';
-import AuthOptions from '@/app/auth/AuthOptions';
-import AssigneeSelect from './AssigneeSelect';
+import prisma from "@/prisma/client";
+import { Box, Flex, Grid } from "@radix-ui/themes";
+import { notFound } from "next/navigation";
+import EditIssueButton from "./EditIssueButton";
+import IssueDetails from "./IssueDetails";
+import DeleteIssueButton from "./DeleteIssueButton";
+import { getServerSession } from "next-auth";
+import AuthOptions from "@/app/auth/AuthOptions";
+import AssigneeSelect from "./AssigneeSelect";
 
 interface Props {
   params: { id: string };
-};
+}
 
 const IssueDetailPage = async ({ params }: Props) => {
-
   const session = await getServerSession(AuthOptions);
 
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
   });
 
-  if (!issue)
-    notFound();
+  if (!issue) notFound();
 
   return (
     <Grid columns={{ initial: "1", sm: "5" }} gap="5">
-      <Box className='md:col-span-4'>
+      <Box className="md:col-span-4">
         <IssueDetails issue={issue} />
       </Box>
       {session && (
         <Box>
           <Flex direction="column" gap="4">
-            <AssigneeSelect />
+            <AssigneeSelect issue={issue} />
             <EditIssueButton issueId={issue.id} />
             <DeleteIssueButton issueId={issue.id} />
           </Flex>
         </Box>
-      )
-      }
+      )}
     </Grid>
-  )
-}
+  );
+};
 
-export default IssueDetailPage
+export default IssueDetailPage;
